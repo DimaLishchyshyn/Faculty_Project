520ae0880d19d5756879a5ffb780dde46bfe7e39
DimaLishchyshyn <lishchyshyn1989@gmail.com>
2020-07-04 08:01:17 +0300

    Added integration tests

--- src/test/java/project/Faculty_Project/FacultyProjectApplicationTests.java
+++ src/test/java/project/Faculty_Project/FacultyProjectApplicationTests.java
@@ -1,13 +1,57 @@
 package project.Faculty_Project;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
-@SpringBootTest
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import project.faculty.dao.UserRepository;
+import project.faculty.domain.User;
+import project.faculty.domain.UserRole;
+import project.faculty.service.UserService;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
 class FacultyProjectApplicationTests {
 
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Test
-	void contextLoads() {
+	public void testSaveUser() {
+		List<User> users = userRepository.findAll();
+		assertThat(users, hasSize(0));
+
+		User user = new User();
+		user.setEmail("1@gmail.com");
+		user.setFirstName("1");
+		user.setLastName("1");
+		user.setPassword("1");
+		user.setPasswordConfirm("1");
+		user.setRole(UserRole.ROLE_USER);
+
+		userService.save(user);
+
+		users = userRepository.findAll();
+		assertThat(users, hasSize(1));
+
+		User userFromDb = users.get(0);
+		assertTrue(userFromDb.getEmail().equals(user.getEmail()));
+		assertTrue(userFromDb.getFirstName().equals(user.getFirstName()));
+		assertTrue(userFromDb.getLastName().equals(user.getLastName()));
+		assertTrue(userFromDb.getRole().equals(user.getRole()));
 	}
+
+
 
 }
